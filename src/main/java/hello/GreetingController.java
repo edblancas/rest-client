20b472d446a17dda979b5ed6272354ed0960f092
Daniel Blancas <edblancas@gmail.com>
2016-08-16 21:06:42 -0500

    Init project with thymeleaf

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @RequestMapping(value = "/quote", method = GET)
+    public String quote(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        model.addAttribute("quote", quote);
+        return "quote";
+    }
+
+}
